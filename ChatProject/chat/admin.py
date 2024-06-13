beaf929ace8d6bd2107899ef9f9cac4ab2080839
amirpvry <amirmahdi.alipooor@gmail.com>
2024-06-13 03:30:34 -0700

    222

--- ChatProject/chat/admin.py
+++ ChatProject/chat/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Chat, Message
 
-# Register your models here.
+@admin.register(Chat)
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_participants')
+
+    def get_participants(self, obj):
+        return ", ".join([participant.username for participant in obj.participants.all()])
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'chat', 'sender', 'content', 'timestamp')
+    list_filter = ('chat', 'sender')
+    search_fields = ('content', 'sender__username')
+
--- ChatProject/chat/views.py
+++ ChatProject/chat/views.py
@@ -1,12 +1,20 @@
 # chat/views.py
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import Chat, Message
 
-def chatPage(request, chat_id, *args, **kwargs):
+from django.shortcuts import render, redirect
+from .models import Chat, Message
+from django.http import HttpResponse
+
+
+def chatPage(request, chat_id):
     if not request.user.is_authenticated:
         return redirect("login-user")
-    chat = get_object_or_404(Chat, id=chat_id)
+    
+    try:
+        chat = Chat.objects.get(id=chat_id)
+    except Chat.DoesNotExist:
+        # Handle the case where Chat object does not exist
+        return HttpResponse("Chat does not exist!", status=404)
+    
     messages = Message.objects.filter(chat=chat).order_by('timestamp')
     context = {'chat': chat, 'messages': messages}
     return render(request, "chat/chatPage.html", context)
-
